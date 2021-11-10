b7e958d73b8a6fd54ef5639240796845ca66ef0c
Rajkumar <23raj2000@gmail.com>
2021-11-10 23:45:50 +0530

    added selection sort

---
+++ selectionSort.py
@@ -0,0 +1,17 @@
+size = int(input("Enter no.of elements: "))
+print("Enter the array elements separated by space: ")
+arr = list(map(int, input().split()))
+
+for j in range(0, len(arr)-1):
+    smallest_ele = arr[j]
+    smallest_ind = j
+    for i in range(j+1, len(arr)):
+        if(arr[i] < smallest_ele):
+            smallest_ele = arr[i]
+            smallest_ind = i
+    arr[j], arr[smallest_ind] = arr[smallest_ind], arr[j]
+
+print(arr)
+
+
+
